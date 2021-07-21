b8f8cc99acc361d9d21f170e1f67dec1ee9a2321
Ben Talbot <b.talbot@qut.edu.au>
2021-07-21 16:27:17 +1000

    Create a starting setup.py for pip package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setup(name='bottom-up-attention',
+      version='0.9.0',
+      author='Ben Talbot',
+      author_email='b.talbot@qut.edu.au',
+      url='https://github.com/best-of-acrv/bottom-up-attention',
+      description=
+      'Bottom-up attention for image captioning & visual question answering',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      install_requires=['acrv_datasets'],
+      entry_points={
+          'console_scripts': [
+              'bottom-up-attention=bottom_up_attention.__main__:main'
+          ]
+      },
+      classifiers=(
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ))
