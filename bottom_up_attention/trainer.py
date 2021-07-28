c3dd7bdb1d2f58c37ae2679083da27035cff7e10
Ben Talbot <b.talbot@qut.edu.au>
2021-07-28 19:01:42 +1000

    Remove args embedding from VQA training

--- bottom_up_attention/trainer.py
+++ bottom_up_attention/trainer.py
@@ -9,25 +9,21 @@ def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
 
+
 class Trainer(nn.Module):
-    def __init__(self, args):
+
+    def __init__(self, output_directory):
         super().__init__()
 
-        # create directory for outputs
-        self.eval_interval = args.eval_interval
-        self.display_interval = args.display_interval
-        self.snapshot_interval = args.snapshot_interval
-        self.load_directory = args.load_directory
-        self.save_directory = args.save_directory
+        # Declare directory for outputs
+        self.output_directory = output_directory
 
-        # training parameters
-        self.batch_size = args.batch_size
-        self.max_epochs = args.max_epochs
-
-    def train_captioning(self, args, model, dataset):
-
+    def _train_captioning(self, model, dataset):
         train_dataset = dataset['train']
-        dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=1)
+        dataloader = DataLoader(train_dataset,
+                                batch_size=self.batch_size,
+                                shuffle=True,
+                                num_workers=1)
 
         # start training
         model.train()
@@ -66,7 +62,8 @@ class Trainer(nn.Module):
                     caption_len = caption_len.cuda()
 
                 # forward inference to compute logits
-                scores, caps_sorted, decode_lengths, sort_ind = model(features, caption, caption_len)
+                scores, caps_sorted, decode_lengths, sort_ind = model(
+                    features, caption, caption_len)
 
                 # fit to model
                 loss, top5 = model.fit(scores, caps_sorted, decode_lengths)
@@ -80,13 +77,17 @@ class Trainer(nn.Module):
 
                 # display current training loss
                 if (curr_iteration + 1) % self.display_interval == 0:
-                    print('Epoch: [{0}][{1}/{2}]\t'
-                          'Batch Time: {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                          'Loss: {loss.val:.4f} ({loss.avg:.4f})\t'
-                          'Top-5 Accuracy: {top5.val:.3f} ({top5.avg:.3f})'.format(curr_epoch, epoch_iteration + 1, len(dataloader),
-                                                                                   batch_time=batch_time,
-                                                                                   loss=losses,
-                                                                                   top5=top5accs))
+                    print(
+                        'Epoch: [{0}][{1}/{2}]\t'
+                        'Batch Time: {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                        'Loss: {loss.val:.4f} ({loss.avg:.4f})\t'
+                        'Top-5 Accuracy: {top5.val:.3f} ({top5.avg:.3f})'.
+                        format(curr_epoch,
+                               epoch_iteration + 1,
+                               len(dataloader),
+                               batch_time=batch_time,
+                               loss=losses,
+                               top5=top5accs))
                 epoch_iteration += 1
                 curr_iteration += 1
 
@@ -102,7 +103,8 @@ class Trainer(nn.Module):
             best_bleu4 = max(recent_bleu4, best_bleu4)
             if not is_best:
                 epochs_since_improvement += 1
-                print("\nEpochs since last improvement: %d\n" % (epochs_since_improvement))
+                print("\nEpochs since last improvement: %d\n" %
+                      (epochs_since_improvement))
             else:
                 # save our best model after each epoch
                 epochs_since_improvement = 0
@@ -111,15 +113,14 @@ class Trainer(nn.Module):
             # update current epoch
             curr_epoch += 1
 
-
-    def train_vqa(self, args, model, dataset):
-
+    def _train_vqa(self, model, dataset, *, batch_size, display_interval,
+                   eval_interval, max_epochs, snapshot_interval):
         # setup dataloader for VQA training set
         train_dataset = dataset['train']
-        dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=1)
-
-        # make save directory
-        os.makedirs(self.save_directory, exist_ok=True)
+        dataloader = DataLoader(train_dataset,
+                                batch_size=batch_size,
+                                shuffle=True,
+                                num_workers=1)
 
         # define loss criterion
         criterion = nn.BCEWithLogitsLoss()
@@ -132,7 +133,7 @@ class Trainer(nn.Module):
         curr_epoch = 0
         curr_iteration = 0
         best_eval_score = 0
-        for epoch in range(self.max_epochs):
+        for epoch in range(max_epochs):
 
             # reset iteration for after each epoch
             epoch_iteration = 0
@@ -153,46 +154,40 @@ class Trainer(nn.Module):
 
                 # compute loss and score
                 mean_loss = model.fit(output, answer, criterion)
-                batch_score = model.compute_score_with_logits(output, answer).sum()
+                batch_score = model.compute_score_with_logits(output,
+                                                              answer).sum()
 
                 # Keep track of metrics
                 stop = timeit.default_timer()
 
                 # display current training loss
-                if (curr_iteration + 1) % self.display_interval == 0:
+                if (curr_iteration + 1) % display_interval == 0:
                     stop = timeit.default_timer()
 
                     print('Epoch: [{0}][{1}/{2}]\t'
                           'Batch Time: {3}\t'
                           'Loss: {4}\t'
-                          'Score: {5})'.format(curr_epoch, epoch_iteration + 1, len(dataloader),
-                                                                          stop-start,
-                                                                          mean_loss.item(),
-                                                                          batch_score))
+                          'Score: {5})'.format(curr_epoch, epoch_iteration + 1,
+                                               len(dataloader), stop - start,
+                                               mean_loss.item(), batch_score))
 
-                    # print('Epoch: [{0}][{1}/{2}]\t'
-                    #       [Time: {:3f}] [Lr: {}] [loss: {:4f}] [score: {:4f}]'.format(curr_epoch,
-                    #                                                                                           curr_iteration + 1,
-                    #                                                                                           stop - start,
-                    #                                                                                           get_lr(model.optimiser),
-                    #                                                                                           mean_loss.item(),
-                    #                                                                                           batch_score))
-
+                # Save the model if necessary & move to next iteration
+                if (curr_iteration + 1) % snapshot_interval == 0:
+                    model.save(curr_iteration + 1, self.output_directory)
                 start = stop
-
                 epoch_iteration += 1
                 curr_iteration += 1
 
             # evaluate model by computing mean IU
-            if (epoch + 1) % self.eval_interval == 0 and dataset['val']:
+            if (epoch + 1) % eval_interval == 0 and dataset['val']:
 
                 # set model to eval first
                 model.eval()
                 eval_score, bound = model.validate(dataset['val'])
-                print('[Epoch: {}] [Iter: {}] [eval_score: {:4f}] [upper_bound: {:4f}]'.format(curr_epoch,
-                                                                                               curr_iteration + 1,
-                                                                                               eval_score*100,
-                                                                                               bound*100))
+                print(
+                    '[Epoch: {}] [Iter: {}] [eval_score: {:4f}] [upper_bound: {:4f}]'
+                    .format(curr_epoch, curr_iteration + 1, eval_score * 100,
+                            bound * 100))
                 model.train()
 
             # save our best model after each epoch
@@ -203,7 +198,8 @@ class Trainer(nn.Module):
             # update current epoch
             curr_epoch += 1
 
-
-
-
-
+    def train(self, task, model, dataset):
+        if not os.path.exists(self.output_directory):
+            os.makedirs(self.output_directory, exist_ok=True)
+        (self._train_captioning if task == 'captioning' else self._train_vqa)(
+            model, dataset)
